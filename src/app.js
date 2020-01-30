bdb56c3fbf42e814b6be3a8f7908cd0f31745e85
Gamonpo <gamonpo@outlook.com>
2020-01-30 09:37:48 -0300

    Cria metodo mw, routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const routes = require('./routes');
 
 class App {
   constructor() {
     this.server = express();
   }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+  
+  routes() {
+    this.server.use(routes);
+  }
 }
+
