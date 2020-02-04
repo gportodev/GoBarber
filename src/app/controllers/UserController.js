0252a71fea192325fc853d9e3a3fd2c79a6bace5
Gamonpo <gamonpo@outlook.com>
2020-02-04 11:39:27 -0300

    Cria feature de cadastro

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,11 @@
 import User from '../models/User';
 
-class UserController {}
+class UserController {
+  async store(req, res) {
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+}
 
 export default new UserController();
