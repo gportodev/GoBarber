078d10adcc38bf3be91e8d8eac2daf8d761c7bcb
Gamonpo <gamonpo@outlook.com>
2020-02-17 15:01:12 -0300

    Ajuste mensagemns

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -15,7 +15,7 @@ class SessionController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails. ' });
+      return res.status(400).json({ error: 'Dados incorretos! ' });
     }
 
     const { email, password } = req.body;
@@ -23,11 +23,11 @@ class SessionController {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found' });
+      return res.status(401).json({ error: 'Usuário não existe!' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Senha errada!' });
     }
 
     const { id, name } = user;
