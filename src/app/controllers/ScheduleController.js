be28baf09a69263b9dd76781eafcad7e364cf37f
gamonpo <gamonpo@outlook.com>
2020-03-11 16:53:47 -0300

    feat(agendamento):lista agendamentos do dia

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,38 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import Sequelize from 'sequelize';
+
+import Appointment from '../models/Appointment';
+
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'Usuário não é um provider!' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const { Op } = Sequelize;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
