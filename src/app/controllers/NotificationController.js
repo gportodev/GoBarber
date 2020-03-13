82a155ed73a1e8e6c841846500f2fcae0e6f8e0c
gamonpo <gamonpo@outlook.com>
2020-03-13 08:47:35 -0300

    feat(notification):notificacao sobre agendamento

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,25 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    const checkisProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkisProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Somente um provider ver agendamentos!' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+}
+export default new NotificationController();
