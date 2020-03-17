1a04b1194d7167647bd6cd39b985b70655751366
gamonpo <gamonpo@outlook.com>
2020-03-17 09:06:11 -0300

    feat(agendamento):envio de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'fa90b1b70d95ca',
+    pass: 'e5581be2d9021d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
